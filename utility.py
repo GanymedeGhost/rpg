a890fc51e39dfe61c33995d035c2edd66705e2ae
DESKTOP-EVT3VC5\vince <vincentcaputa@gmail.com>
2017-03-02 23:23:51 -0500

    Added colorize method to utility

--- utility.py
+++ utility.py
@@ -12,6 +12,21 @@ def scale_tuple(o, f):
     p1 = o[1] * f[1]
     return p0, p1
 
+def colorize(image, newColor):
+    """
+    Create a "colorized" copy of a surface (replaces RGB values with the given color, preserving the per-pixel alphas of
+    original).
+    :param image: Surface to create a colorized copy of
+    :param newColor: RGB color to use (original alpha values are preserved)
+    :return: New colorized Surface instance
+    """
+    image = image.copy().convert_alpha()
+    image.fill((0,0,0,255), None, pygame.BLEND_RGBA_MULT)
+    # zero out RGB values
+    image.fill(newColor[:] + (0,), None, pygame.BLEND_RGBA_ADD)
+
+    return image
+
 def log(string = "", level = g.LogLevel.DEBUG):
     if g.LOG_FILTER[level]:
         print (string)
